e30e2972d7db868580cd14915433188cf7ca2bb4
jennyhappy722 <u10316052@go.utaipei.edu.tw>
2016-04-01 22:10:38 +0800

    add can excute up 1

--- Test.java
+++ Test.java
@@ -13,13 +13,18 @@ public class Test extends Application{
 	@Override
 	public void start(Stage primaryStage){
 		LineHW linehw=new LineHW();		
-		linehw.pplaying();
+
 
 		Scene scene = new Scene(linehw, 300, 300);
     		primaryStage.setTitle("game"); // Set the stage title
     		primaryStage.setScene(scene); // Place the scene in the stage
+    		scene.setOnMouseClicked(e ->{
+		linehw.rootu();
+		linehw.animation1(linehw.getcircle(),linehw.getr1(),linehw.getr2(),linehw.getr3(),linehw.getr4(),linehw.getr5(),linehw.getr6(),linehw.getr7(),linehw.getr8(),linehw.getr9());
+		});
+    
     		primaryStage.show();
-		linehw.animation1();
+
 
 	}
 }
