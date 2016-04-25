fa4460c3255934e0f3a9ddeac5570aca6d086e1a
jennyhappy722 <u10316052@go.utaipei.edu.tw>
2016-04-25 10:21:08 +0800

    Update LineHW.java

--- LineHW.java
+++ LineHW.java
@@ -1,5 +1,3 @@
-//u10316052
-//import package
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.*;
@@ -14,44 +12,32 @@ import javafx.beans.value.WritableObjectValue;
 import javafx.animation.*;
 import java.util.*;
 import javafx.event.*;
-//class
-
-
-public class LineHW extends Pane{
-	//start dot location
-	double x=150;
+public class LineHW extends Pane{//class
+	double x=150;//start dot location
 	double y=50;
-	//start dot
-	public Circle circle;
+	public Circle circle;//start dot
 	Timeline animation;
-	//add path location dot 36
-	Circle D1 = new Circle(150,60,2);
-
+	Circle D1 = new Circle(150,60,2);//add path location dot 36
 	Circle D2 = new Circle(140,72,2);
 	Circle D3 = new Circle(160,72,2);
-
 	Circle D4 = new Circle(130,90,2);
 	Circle D5 = new Circle(150,90,2);
 	Circle D6 = new Circle(170,90,2);
-
 	Circle D7 = new Circle(120,108,2);
 	Circle D8 = new Circle(140,108,2);
 	Circle D9 = new Circle(160,108,2);
 	Circle D10 = new Circle(180,108,2);
-
 	Circle D11 = new Circle(110,126,2);
 	Circle D12 = new Circle(130,126,2);
 	Circle D13 = new Circle(150,126,2);
 	Circle D14 = new Circle(170,126,2);
 	Circle D15 = new Circle(190,126,2);
-
 	Circle D16 = new Circle(100,144,2);
 	Circle D17 = new Circle(120,144,2);
 	Circle D18 = new Circle(140,144,2);
 	Circle D19 = new Circle(160,144,2);
 	Circle D20 = new Circle(180,144,2);
 	Circle D21 = new Circle(200,144,2);
-
 	Circle D22 = new Circle(90,162,2);
 	Circle D23 = new Circle(110,162,2);
 	Circle D24 = new Circle(130,162,2);
@@ -59,7 +45,6 @@ public class LineHW extends Pane{
 	Circle D26 = new Circle(170,162,2);
 	Circle D27 = new Circle(190,162,2);
 	Circle D28 = new Circle(210,162,2);
-
 	Circle D29 = new Circle(80,180,2);
 	Circle D30 = new Circle(100,180,2);
 	Circle D31 = new Circle(120,180,2);
@@ -68,7 +53,6 @@ public class LineHW extends Pane{
 	Circle D34 = new Circle(180,180,2);
 	Circle D35 = new Circle(200,180,2);
 	Circle D36 = new Circle(220,180,2);
-	
 	Circle D37 = new Circle(80,238,2);
 	Circle D38 = new Circle(100,238,2);
 	Circle D39 = new Circle(120,238,2);
@@ -77,11 +61,9 @@ public class LineHW extends Pane{
 	Circle D42 = new Circle(180,238,2);
 	Circle D43 = new Circle(200,238,2);
 	Circle D44 = new Circle(220,238,2);	
-	
-	//add path dot
-	Circle r1 = new Circle(150,160,2);
+	Circle r1 = new Circle(150,160,2);//add path dot
 	Circle r2 = new Circle(120,180,2);
-	Circle r3 = new Circle(140,180,2);
+	Circle r3 = new Circle(140,180,2);	
 	Circle r4 = new Circle(160,180,2);
 	Circle r5 = new Circle(180,180,2);
 	Circle r6 = new Circle(200,180,2);
@@ -96,11 +78,8 @@ public class LineHW extends Pane{
 	int f=0;
 	int g=0;
 	int h=0;	
-	
-	//paint shape
 	public LineHW(){
-		//create line 
-		Line line=new Line(130,50,130,80);
+		Line line=new Line(130,50,130,80);//create line 
 		Line line1=new Line(170,50,170,80);
 		Line line2=new Line(130,80,70,180);
 		Line line3=new Line(170,80,230,180);
@@ -114,52 +93,39 @@ public class LineHW extends Pane{
 		Line line11=new Line(170,180,170,240);
 		Line line12=new Line(190,180,190,240);
 		Line line13=new Line(210,180,210,240);
-		//create dot 28
-		Ellipse e1=new Ellipse(90,180,5,5);
+		Ellipse e1=new Ellipse(90,180,5,5);//create dot 28
 		Ellipse e2=new Ellipse(110,180,5,5);
 		Ellipse e3=new Ellipse(130,180,5,5);
 		Ellipse e4=new Ellipse(150,180,5,5);
 		Ellipse e5=new Ellipse(170,180,5,5);
 		Ellipse e6=new Ellipse(190,180,5,5);
 		Ellipse e7=new Ellipse(210,180,5,5);
-
 		Ellipse e8=new Ellipse(100,162,5,5);
 		Ellipse e9=new Ellipse(120,162,5,5);
 		Ellipse e10=new Ellipse(140,162,5,5);
 		Ellipse e11=new Ellipse(160,162,5,5);
 		Ellipse e12=new Ellipse(180,162,5,5);
 		Ellipse e13=new Ellipse(200,162,5,5);
-
 		Ellipse e14=new Ellipse(110,144,5,5);
 		Ellipse e15=new Ellipse(130,144,5,5);
 		Ellipse e16=new Ellipse(150,144,5,5);
 		Ellipse e17=new Ellipse(170,144,5,5);
 		Ellipse e18=new Ellipse(190,144,5,5);
-
 		Ellipse e19=new Ellipse(120,126,5,5);
 		Ellipse e20=new Ellipse(140,126,5,5);
 		Ellipse e21=new Ellipse(160,126,5,5);
 		Ellipse e22=new Ellipse(180,126,5,5);
-
 		Ellipse e23=new Ellipse(130,108,5,5);
 		Ellipse e24=new Ellipse(150,108,5,5);
 		Ellipse e25=new Ellipse(170,108,5,5);
-
 		Ellipse e26=new Ellipse(140,90,5,5);
 		Ellipse e27=new Ellipse(160,90,5,5);
-
 		Ellipse e28=new Ellipse(150,72,5,5);
-		
-		
-
 		//add to pane
 		getChildren().addAll(line,line1,line2,line3,line4,line5,line6,line7,line8,line9,line10,line11,line12,line13,e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12,e13,e14,e15,e16,e17,e18,e19,e20,e21,e22,e23,e24,e25,e26,e27,e28);
-
 	}
-	//path animation
 	public void animation1(Circle circle,Circle Dots1 ,Circle Dots2,Circle Dots3,Circle Dots4,Circle Dots5,Circle Dots6,Circle Dots7,Circle Dots8,Circle Dots9){
-    		// Create an animation for moving the ball
-		circle=new Circle(x,y,2);
+		circle=new Circle(x,y,2);// Create an animation for moving the ball
     		Path path = new Path();
     		path.getElements().add(new MoveTo(circle.getCenterX(),circle.getCenterY()));
    		path.getElements().add(new LineTo(Dots1.getCenterX(), Dots1.getCenterY()));
@@ -173,16 +139,9 @@ public class LineHW extends Pane{
 		path.getElements().add(new LineTo(Dots9.getCenterX(), Dots9.getCenterY()));
 		getChildren().addAll(circle);
     		PathTransition pathtransition = new PathTransition(Duration.millis(1000),path,circle);//time,path,object
-    		/*pathtransition.setDuration(Duration.seconds(1));
-    		pathtransition.setNode(circle);
-    		pathtransition.setPath(path);*/
-    		//pathtransition.setOrientation(OrientationType.ORTHOGONAL_TO_TANGENT);   	  
     		pathtransition.play();
 	}
-
-	
-	//path random
-	public void rootu(){
+	public void rootu(){//path random
 		int r = new Random().nextInt(2)+1;
 		r1=D1;
 		//---------------------------------------------------------------------r1
@@ -190,7 +149,6 @@ public class LineHW extends Pane{
 			r2=D2;
 		else
 			r2=D3;
-		
 		//---------------------------------------------------------------------r2
 		if(r2==D2){
 			r = new Random().nextInt(2)+1;
@@ -216,7 +174,6 @@ public class LineHW extends Pane{
 			else
 				r4=D8;
 		}
-
 		if(r3==D5){
 			r = new Random().nextInt(2)+1;
 			if(r==2)
@@ -224,7 +181,6 @@ public class LineHW extends Pane{
 			else
 				r4=D8;
 		}
-
 		if(r3==D6){
 			r = new Random().nextInt(2)+1;
 			if(r==2)
@@ -361,7 +317,6 @@ public class LineHW extends Pane{
 			else
 				r8=D30;
 		}
-
 		if(r7==D23){
 			r = new Random().nextInt(2)+1;
 			if(r==2)
@@ -377,7 +332,6 @@ public class LineHW extends Pane{
 			else
 				r8=D32;
 		}
-
 		if(r7==D25){
 			r = new Random().nextInt(2)+1;
 			if(r==2)
@@ -401,7 +355,6 @@ public class LineHW extends Pane{
 			else
 				r8=D34;
 		}
-
 		if(r7==D28){
 			r = new Random().nextInt(2)+1;
 			if(r==2)
@@ -409,7 +362,7 @@ public class LineHW extends Pane{
 			else
 				r8=D36;
 		}
-				//--------------------------------------------------------------r8
+		//--------------------------------------------------------------r8
 		if(r8==D29){			
 			r9=D37;
 			a++;
@@ -460,7 +413,6 @@ public class LineHW extends Pane{
 			break;
 			}			
 		}
-
 		if(r8==D34){			
 			r9=D42;
 			f++;
@@ -470,7 +422,6 @@ public class LineHW extends Pane{
 			break;
 			}			
 		}
-
 		if(r8==D35){			
 			r9=D43;
 			g++;
@@ -480,7 +431,6 @@ public class LineHW extends Pane{
 			break;
 			}			
 		}
-
 		if(r8==D36){			
 			r9=D44;
 			h++;
@@ -490,61 +440,35 @@ public class LineHW extends Pane{
 			break;
 			}			
 		}
-				
-	
-
-		
 	}
-	//get r location
 	public Circle getr1(){
-
 		return r1;
 	}
-	//get r location
 	public Circle getr2(){
-
 		return r2;
 	}
-
-	//get r location
 	public Circle getr3(){
-
 		return r3;
 	}
-	//get r location
 	public Circle getr4(){
-
 		return r4;
 	}
-	//get r location
 	public Circle getr5(){
-
 		return r5;
 	}
-	//get r location
 	public Circle getr6(){
-
 		return r6;
 	}
-	//get r location
 	public Circle getr7(){
-
 		return r7;
 	}
-	//get r location
 	public Circle getr8(){
-
 		return r8;
 	}
 	public Circle getr9(){
-
 		return r9;
 	}
 	public Circle getcircle(){
-
 		return circle;
 	}
-
-
-
 }
